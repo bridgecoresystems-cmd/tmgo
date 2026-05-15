ecbc0f1b915de1f07d2dd32867be79338db9a564
Batyr Akmuradov <bridgecoresystems@gmail.com>
2026-05-15 05:59:43 +0500

    refactor: modularize i18n locales by splitting domain-specific files per language

--- packages/frontend/i18n/i18n.config.ts
+++ packages/frontend/i18n/i18n.config.ts
@@ -1,15 +1,54 @@
-import ru from '../locales/ru.json'
-import en from '../locales/en.json'
-import tk from '../locales/tk.json'
+import ruCommon from '../locales/ru/common.json'
+import ruLanding from '../locales/ru/landing.json'
+import ruAdmin from '../locales/ru/admin.json'
+import ruClient from '../locales/ru/client.json'
+import ruDriver from '../locales/ru/driver.json'
+
+import enCommon from '../locales/en/common.json'
+import enLanding from '../locales/en/landing.json'
+import enAdmin from '../locales/en/admin.json'
+import enClient from '../locales/en/client.json'
+import enDriver from '../locales/en/driver.json'
+
+import tkCommon from '../locales/tk/common.json'
+import tkLanding from '../locales/tk/landing.json'
+import tkAdmin from '../locales/tk/admin.json'
+import tkClient from '../locales/tk/client.json'
+import tkDriver from '../locales/tk/driver.json'
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'ru',
   fallbackLocale: 'ru',
   messages: {
-    ru,
-    en,
-    tk,
-    'tk-TM': tk
+    ru: {
+      ...ruCommon,
+      ...ruLanding,
+      admin: ruAdmin,
+      client: ruClient,
+      driver: ruDriver
+    },
+    en: {
+      ...enCommon,
+      ...enLanding,
+      admin: enAdmin,
+      client: enClient,
+      driver: enDriver
+    },
+    tk: {
+      ...tkCommon,
+      ...tkLanding,
+      admin: tkAdmin,
+      client: tkClient,
+      driver: tkDriver
+    },
+    'tk-TM': {
+      ...tkCommon,
+      ...tkLanding,
+      admin: tkAdmin,
+      client: tkClient,
+      driver: tkDriver
+    }
   }
 }))
+
